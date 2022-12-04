3b1db7fed0846baf56a193ccda5baf996c382591
Qingyang Shi <shiqingyang_99@126.com>
2022-12-04 01:34:44 -0800

    draw graph dynamically

--- src/components/heap_growth_graph_v2.tsx
+++ src/components/heap_growth_graph_v2.tsx
@@ -4,60 +4,14 @@ import ReactEcharts from "echarts-for-react";
 import BLeakResults from "../lib/results";
 import { SnapshotSizeSummary } from "../common/interfaces";
 
-const nleakResult = require("./test_data/result.json");
+// const nleakResult = require("./test_data/result.json");
 const BYTES_PER_MB = 1024 * 1024;
 
 interface HeapGrowthGraphProps {
   bleakResults: BLeakResults;
 }
 
-const { heapStats } = nleakResult;
-const option = {
-  tooltip: {
-    trigger: 'axis'
-  },
-  legend: {
-    data: heapStats.map((_, i) => `snapshot-${i+1}`)
-  },
-  grid: {
-    left: '3%',
-    right: '4%',
-    bottom: '3%',
-    containLabel: true
-  },
-  toolbox: {
-    feature: {
-      saveAsImage: {}
-    }
-  },
-  xAxis: {
-	name: 'Round Trips Completed',
-	nameLocation: 'middle',
-	nameGap: 17,
-    type: 'category',
-    boundaryGap: false,
-    data: heapStats.map((_, i) => `${i+1}`)
-  },
-  yAxis: {
-	name: 'Live Heap Size',
-	nameLocation: 'middle',
-    nameGap: 50,
-    type: 'value',
-	min: Math.floor(Math.min.apply(null,heapStats.map((h) => Math.floor(h.totalSize/ BYTES_PER_MB))))*0.8,
-	max: Math.ceil(Math.ceil(Math.max.apply(null,heapStats.map((h) => Math.ceil(h.totalSize/ BYTES_PER_MB))))*1.2),
-	axisLabel: {
-		formatter: '{value} MB'
-	}
-  },
-  series: [
-    {
-      name: 'Total Size',
-      type: 'line',
-      stack: 'Total',
-      data: heapStats.map((h) => h.totalSize/ BYTES_PER_MB)
-    }
-  ]
-};
+
 
 class HeapGrowthGraphV2 extends React.Component<HeapGrowthGraphProps> {
   public componentWillMount() {
@@ -73,6 +27,55 @@ class HeapGrowthGraphV2 extends React.Component<HeapGrowthGraphProps> {
   }
 
   public render() {
+	const heapStats = this.props.bleakResults.toJSON().heapStats;
+	const option = {
+	tooltip: {
+		trigger: 'axis'
+	},
+	legend: {
+		data: heapStats.map((_, i) => `snapshot-${i+1}`)
+	},
+	grid: {
+		left: '3%',
+		right: '4%',
+		bottom: '3%',
+		containLabel: true
+	},
+	toolbox: {
+		feature: {
+		saveAsImage: {}
+		}
+	},
+	xAxis: {
+		name: 'Round Trips Completed',
+		nameLocation: 'middle',
+		nameGap: 17,
+		type: 'category',
+		boundaryGap: false,
+		data: heapStats.map((_, i) => `${i+1}`)
+	},
+	yAxis: {
+		name: 'Live Heap Size',
+		nameLocation: 'middle',
+		nameGap: 35,
+		type: 'value',
+		min: Math.floor(Math.min.apply(null,heapStats.map((h) => Math.floor(h.totalSize/ BYTES_PER_MB))))*0.8,
+		max: Math.ceil(Math.ceil(Math.max.apply(null,heapStats.map((h) => Math.ceil(h.totalSize/ BYTES_PER_MB))))*1.2),
+		axisLabel: {
+			formatter: '{value} MB'
+		}
+	},
+	series: [
+		{
+		name: 'Total Size',
+		type: 'line',
+		stack: 'Total',
+		data: heapStats.map((h) => h.totalSize/ BYTES_PER_MB)
+		}
+	]
+	};
+
+
     return <div>
       <ReactEcharts option={option} />
     </div>
--- src/components/summary_pie_graph.tsx
+++ src/components/summary_pie_graph.tsx
@@ -4,55 +4,13 @@ import ReactEcharts from "echarts-for-react";
 import BLeakResults from "../lib/results";
 import { SnapshotSizeSummary } from "../common/interfaces";
 
-const nleakResult = require("./test_data/result.json");
+// const nleakResult = require("./test_data/result.json");
 
 interface HeapGrowthGraphProps {
 	bleakResults: BLeakResults;
 }
 
-const { heapStats } = nleakResult;
-const option = {
-  tooltip: {
-    trigger: 'item'
-  },
-  dataset:{
-	source:[
-		['stack','1','2','3'],
-		['Hidden Size'].concat(heapStats.map((h) => h.hiddenSize)),
-		['Array Size'].concat(heapStats.map((h) => h.arraySize)),
-		['Array Size'].concat(heapStats.map((h) => h.arraySize)), 
-		['String Size'].concat(heapStats.map((h) => h.stringSize)),
-		['Object Size'].concat(heapStats.map((h) => h.objectSize)), 
-		['Code Size'].concat(heapStats.map((h) => h.codeSize)),
-		['Closure Size'].concat(heapStats.map((h) => h.closureSize)), 
-		['Regexp Size'].concat(heapStats.map((h) => h.regexpSize)), 
-		['Heap Number Size'].concat(heapStats.map((h) => h.heapNumberSize)), 
-		['Native Size'].concat(heapStats.map((h) => h.nativeSize)),
-		['Synthetic Size'].concat(heapStats.map((h) => h.syntheticSize)), 
-		['ConsString Size'].concat(heapStats.map((h) => h.consStringSize)),
-		['Sliced String Size'].concat(heapStats.map((h) => h.slicedStringSize)),
-		['Symbol Size'].concat(heapStats.map((h) => h.symbolSize)),
-		['Unknown Size'].concat(heapStats.map((h) => h.unknownSize))
-	]
-  },
-  series: [
-	{
-	  type: 'pie',
-	  radius: '100%',
-	  encode:{
-		itemName:'stack',
-		value: '3'
-	  },
-	  emphasis: {
-		itemStyle: {
-			shadowBlur: 10,
-			shadowOffsetX: 0,
-			shadowColor: 'rgba(0, 0, 0, 0.5)'
-		  }
-		}
-	}
-  ]
-};
+
 
 class SummaryPieGraph extends React.Component<HeapGrowthGraphProps> {
   public componentWillMount() {
@@ -68,6 +26,50 @@ class SummaryPieGraph extends React.Component<HeapGrowthGraphProps> {
   }
 
   public render() {
+	const heapStats = this.props.bleakResults.toJSON().heapStats;
+	const option = {
+	tooltip: {
+		trigger: 'item'
+	},
+	dataset:{
+		source:[
+			['stack','1','2','3'],
+			[...['Hidden Size'],...heapStats.map((h) => h.hiddenSize)],
+			[...['Array Size'],...heapStats.map((h) => h.arraySize)], 
+			[...['String Size'],...heapStats.map((h) => h.stringSize)],
+			[...['Object Size'],...heapStats.map((h) => h.objectSize)], 
+			[...['Code Size'],...heapStats.map((h) => h.codeSize)],
+			[...['Closure Size'],...heapStats.map((h) => h.closureSize)], 
+			[...['Regexp Size'],...heapStats.map((h) => h.regexpSize)], 
+			[...['Heap Number Size'],...heapStats.map((h) => h.heapNumberSize)], 
+			[...['Native Size'],...heapStats.map((h) => h.nativeSize)],
+			[...['Synthetic Size'],...heapStats.map((h) => h.syntheticSize)], 
+			[...['ConsString Size'],...heapStats.map((h) => h.consStringSize)],
+			[...['Sliced String Size'],...heapStats.map((h) => h.slicedStringSize)],
+			[...['Symbol Size'],...heapStats.map((h) => h.symbolSize)],
+			[...['Unknown Size'],...heapStats.map((h) => h.unknownSize)]
+		]
+	},
+	series: [
+		{
+		type: 'pie',
+		radius: '100%',
+		encode:{
+			itemName:'stack',
+			value: '3'
+		},
+		emphasis: {
+			itemStyle: {
+				shadowBlur: 10,
+				shadowOffsetX: 0,
+				shadowColor: 'rgba(0, 0, 0, 0.5)'
+			}
+			}
+		}
+	]
+	};
+
+
     return <div>
       <ReactEcharts option={option} />
     </div>
